0ec93ac86dc5f44a061f1092682d99db0a15a62b
BabySid <blackfox1983@163.com>
2022-12-06 15:08:54 +0800

    update code

--- fat_controller/metastore/thomas.go
+++ fat_controller/metastore/thomas.go
@@ -188,8 +188,7 @@ func (ms *metaStore) DropThomas(thomas *sodor.ThomasInfo) error {
 func (ms *metaStore) SelectValidThomas(host string) (*Thomas, error) {
 	var thomas Thomas
 	thomas.Host = host
-	//ms.db.
-	if rs := ms.db.Scopes(filterValidThomas).Order("id desc").Limit(1).Find(&thomas); rs.Error != nil {
+	if rs := ms.db.Scopes(filterValidThomas).Order("id desc").Where(thomas).Limit(1).Find(&thomas); rs.Error != nil {
 		return nil, rs.Error
 	}
 	return &thomas, nil
